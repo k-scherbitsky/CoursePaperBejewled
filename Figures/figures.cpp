ed63498b150ba90b27d5ab7aeb061aac7ad480e1
k-scherbitsky <k.scherbitsky@gmail.com>
2017-11-27 03:20:55 +0300

    update figures.cpp

--- Figures/figures.cpp
+++ Figures/figures.cpp
@@ -7,16 +7,17 @@
 #include "figures.h"
 #include "../BasicFunc/basicFigures.h"
 
-void drawBlueGem() {
+int drawBlueGem() {
+    
     glBegin(GL_POLYGON);
 
-    setColorRGB(176,224,230);
+    setColorRGB(176, 224, 230);
     glVertex3d(-0.25, 0.2, 0.1);
     glVertex3d(-0.2, 0.25, 0.1);
-    setColorRGB(0,191,255);
+    setColorRGB(0, 191, 255);
     glVertex3d(0.2, 0.25, 0.1);
     glVertex3d(0.25, 0.2, 0.1);
-    setColorRGB(0,0,128);
+    setColorRGB(0, 0, 128);
     glVertex3d(0, -0.2, 0.1);
 
     glEnd();
@@ -33,23 +34,25 @@ void drawBlueGem() {
     glVertex3d(0, -0.2, 0.1);
 
     glEnd();
+    return 1;
 }
 
-void drawGreenGem() {
+int drawGreenGem() {
+    
 
     glBegin(GL_POLYGON);
 
-    setColorRGB(0,100,0);
+    setColorRGB(0, 100, 0);
     glVertex3d(-0.25, 0.2, 0.1);
     glVertex3d(-0.2, 0.25, 0.1);
     glVertex3d(0.2, 0.25, 0.1);
     glVertex3d(0.25, 0.2, 0.1);
 
-    setColorRGB(144,238,144);
+    setColorRGB(144, 238, 144);
     glVertex3d(0.25, -0.2, 0.1);
     glVertex3d(0.2, -0.25, 0.1);
 
-    setColorRGB(0,255,127);
+    setColorRGB(0, 255, 127);
     glVertex3d(-0.2, -0.25, 0.1);
     glVertex3d(-0.25, -0.2, 0.1);
 
@@ -70,16 +73,17 @@ void drawGreenGem() {
     glVertex3d(-0.25, -0.2, 0.1);
 
     glEnd();
-
+    return 2;
 }
 
-void drawOrangeGem() {
-    setColorRGB(254,224,0);
+int drawOrangeGem() {
+    
+    setColorRGB(254, 224, 0);
     glBegin(GL_TRIANGLE_FAN);
     glVertex2d(0.0f, 0.0f);
     for (int i = 0; i <= 6; ++i) {
         double rad = i * 2 * M_PI / 6;
-        setColorRGB(245,125,0);
+        setColorRGB(245, 125, 0);
         glVertex3d(cos(rad) * 0.25, sin(rad) * 0.25, 0.1);
     }
     glEnd();
@@ -93,16 +97,18 @@ void drawOrangeGem() {
         glVertex3d(cos(rad) * 0.25, sin(rad) * 0.25, 0.1);
     }
     glEnd();
+    return 3;
 }
 
-void drawPurple(double x0, double y0, double x1, double y1, double x2, double y2, double z) {
+int drawPurpleGem() {
+    
 
-    setColorRGB(236,183,225);
+    setColorRGB(236, 183, 225);
     glBegin(GL_TRIANGLE_FAN);
     glVertex2d(0.0f, 0.0f);
     for (int i = 0; i <= 3; ++i) {
         double rad = i * 2 * M_PI / 3;
-        setColorRGB(128,0,128);
+        setColorRGB(128, 0, 128);
         glVertex3d(cos(rad) * 0.25, sin(rad) * 0.25, 0.1);
     }
     glEnd();
@@ -116,14 +122,15 @@ void drawPurple(double x0, double y0, double x1, double y1, double x2, double y2
         glVertex3d(cos(rad) * 0.25, sin(rad) * 0.25, 0.1);
     }
     glEnd();
-
+    return 4;
 }
 
-void drawRedGem() {
+int drawRedGem() {
+    
 
     glBegin(GL_POLYGON);
 
-    setColorRGB(255,0,0);
+    setColorRGB(255, 0, 0);
     glVertex3d(-0.25, 0.225, 0.1);
     glVertex3d(-0.225, 0.25, 0.1);
 
@@ -131,11 +138,11 @@ void drawRedGem() {
     glVertex3d(0.225, 0.25, 0.1);
     glVertex3d(0.25, 0.225, 0.1);
 
-    setColorRGB(255,228,225);
+    setColorRGB(255, 228, 225);
     glVertex3d(0.25, -0.225, 0.1);
     glVertex3d(0.225, -0.25, 0.1);
 
-    setColorRGB(240,128,128);
+    setColorRGB(240, 128, 128);
     glVertex3d(-0.225, -0.25, 0.1);
     glVertex3d(-0.25, -0.225, 0.1);
 
@@ -156,17 +163,18 @@ void drawRedGem() {
     glVertex3d(-0.25, -0.225, 0.1);
 
     glEnd();
-
+    return 5;
 }
 
-void drawYellowGem() {
+int drawYellowGem() {
+    
 
-    setColorRGB(254,254,162);
+    setColorRGB(254, 254, 162);
     glBegin(GL_TRIANGLE_FAN);
     glVertex2d(0.0f, 0.0f);
     for (int i = 0; i <= 4; ++i) {
         double rad = i * 2 * M_PI / 4;
-        setColorRGB(255,255,0);
+        setColorRGB(255, 255, 0);
         glVertex3d(cos(rad) * 0.25, sin(rad) * 0.25, 0.1);
     }
     glEnd();
@@ -180,6 +188,7 @@ void drawYellowGem() {
         glVertex3d(cos(rad) * 0.25, sin(rad) * 0.25, 0.1);
     }
     glEnd();
-
+    return 6;
 }
+
 
