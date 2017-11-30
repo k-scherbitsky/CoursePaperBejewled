6dddd383cc2013518a0b1adeee225abf1aa2de89
k-scherbitsky <k.scherbitsky@gmail.com>
2017-11-30 22:52:13 +0300

    add renderBitmapSrting

--- BasicFunc/basicFigures.cpp
+++ BasicFunc/basicFigures.cpp
@@ -6,6 +6,7 @@
 #include <cmath>
 #include <cstdio>
 #include "basicFigures.h"
+#include "SOIL/SOIL.h"
 
 GLuint textureID[8];
 struct {
@@ -14,14 +15,7 @@ struct {
     unsigned char *Image;
 } getTexture[8];
 
-void drawLine(double x0, double y0, double x, double y){
-    glBegin(GL_LINE_STIPPLE);
-    glVertex2d(x0, y0);
-    glVertex2d(x, y);
-    glEnd();
-}
-
-void drawRegtangle(double x0, double y0, double x, double y){
+void drawRegtangle(double x0, double y0, double x, double y) {
     glBegin(GL_LINE_LOOP);
     glVertex2d(x0, y0);
     glVertex2d(x, y0);
@@ -30,7 +24,19 @@ void drawRegtangle(double x0, double y0, double x, double y){
     glEnd();
 }
 
-void drawFillRectangle(double x0, double y0, double x, double y, double z){
+void drawFillRectangle(double x0, double y0, double x, double y, double z) {
+    glBegin(GL_POLYGON);
+
+    glVertex3d(x0, y0, z);
+    glVertex3d(x, y0, z);
+
+    glVertex3d(x, y, z);
+    glVertex3d(x0, y, z);
+
+    glEnd();
+}
+
+void createTextureRectangle(double x0, double y0, double x, double y, double z) {
     glBegin(GL_POLYGON);
 
     glTexCoord2d(0, 0);
@@ -48,43 +54,7 @@ void drawFillRectangle(double x0, double y0, double x, double y, double z){
     glEnd();
 }
 
-void createTextureRectangle(double x0, double y0, double x, double y){
-    glBegin(GL_POLYGON);
-    glTexCoord2d(x0, y0);
-    glTexCoord2d(x, y0);
-    glTexCoord2d(x, y);
-    glTexCoord2d(x0, y);
-    glEnd();
-}
-
-void drawTriangle(double x0, double y0, double x1, double y1, double x2, double y2){
-    glBegin(GL_LINE_LOOP);
-    glVertex2d(x0, y0);
-    glVertex2d(x1, y1);
-    glVertex2d(x2, y2);
-    glEnd();
-}
-
-void drawFillTriangle(double x0, double y0, double x1, double y1, double x2, double y2){
-    glBegin(GL_TRIANGLES);
-    glTexCoord2d(0, 0);
-    glVertex2d(x0, y0);
-    glTexCoord2d(0, 1);
-    glVertex2d(x1, y1);
-    glTexCoord2d(1, 0);
-    glVertex2d(x2, y2);
-    glEnd();
-}
-
-void createTextureTriangle(double x0, double y0, double x1, double y1, double x2, double y2){
-    glBegin(GL_TRIANGLES);
-    glTexCoord2d(x0, y0);
-    glTexCoord2d(x1, y1);
-    glTexCoord2d(x2, y2);
-    glEnd();
-}
-
-void drawEllipse(int slices, double x, double y){
+void drawEllipse(int slices, double x, double y) {
     glBegin(GL_LINE_LOOP);
     for (int i = 0; i <= slices; i++) {
         double rad = i * M_PI / 180;
@@ -93,7 +63,7 @@ void drawEllipse(int slices, double x, double y){
     glEnd();
 }
 
-void drawFillEllipse(int slices, double x, double y){
+void drawFillEllipse(int slices, double x, double y) {
     glBegin(GL_TRIANGLE_FAN);
     glVertex2d(0.0f, 0.0f);
     for (int i = 0; i <= slices; ++i) {
@@ -103,17 +73,15 @@ void drawFillEllipse(int slices, double x, double y){
     glEnd();
 }
 
-void createTextureEllipse(int slices, double x, double y);
-
-
-void setColorRGB(double red, double green, double blue){
+void setColorRGBA(double red, double green, double blue, double opacity) {
     double r = red / 255;
     double g = green / 255;
     double b = blue / 255;
-    glColor3d(r, g, b);
+    double op = opacity / 100;
+    glColor4d(r, g, b, op);
 }
 
-void reshapeSize(int w, int h){
+void reshapeSize(int w, int h) {
     if (h == 0) {
         h = 1;
     }
@@ -123,9 +91,10 @@ void reshapeSize(int w, int h){
     glViewport(0, 0, w, h);
     gluPerspective(45, ratio, 1, 1000);
     glMatrixMode(GL_MODELVIEW);
+//    glutReshapeWindow(845, 700);
 }
 
-void createWindow(int args, char **argv, int w, int h){
+void createWindow(int args, char **argv, int w, int h) {
     glutInit(&args, argv);
     glutInitWindowPosition(100, 0);
     glutInitWindowSize(w, h);
@@ -165,13 +134,38 @@ GLuint LoadTexture(char *FileName, int n) {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexEnvi( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE );
+    glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
     gluBuild2DMipmaps(GL_TEXTURE_2D, 4, getTexture[n].W, getTexture[n].H, GL_RGB, GL_UNSIGNED_BYTE,
                       getTexture[n].Image);
     free(getTexture[n].Image);
     fclose(F);
 
     return true;
+
+}
+
+void setBackground() {
+    glBegin(GL_POLYGON);
+
+    setColorRGBA(244, 164, 96, 100);
+    glVertex3d(-5, 5, 0);
+    glVertex3d(5, 5, 0);
+
+    setColorRGBA(192, 192, 192, 100);
+    glVertex3d(5, -5, 0);
+    glVertex3d(-5, -5, 0);
+
+    glEnd();
+}
+
+void renderBitmapString(float x, float y, float z, void *font, const char *string,
+                        double red, double green, double blue) {
+    setColorRGBA(red, green, blue, 100);
+    const char *c;
+    glRasterPos3f(x, y, z);
+    for (c = string; *c != '\0'; c++) {
+        glutBitmapCharacter(font, *c);
+    }
 }
 
 
--- BasicFunc/basicFigures.h
+++ BasicFunc/basicFigures.h
@@ -5,25 +5,24 @@
 #ifndef COURSEWORK_BASICFIGURES_H
 #define COURSEWORK_BASICFIGURES_H
 
-void drawLine(double x0, double y0, double x, double y);
+void drawLine(double x0, double y0, double z0, double x, double y, double z);
 
 void drawRegtangle(double x0, double y0, double x, double y);
 void drawFillRectangle(double x0, double y0, double x, double y, double z);
-void createTextureRectangle(double x0, double y0, double x, double y);
-
-void drawTriangle(double x0, double y0, double x1, double y1, double x2, double y2);
-void drawFillTriangle(double x0, double y0, double x1, double y1, double x2, double y2);
-void createTextureTriangle(double x0, double y0, double x1, double y1, double x2, double y2);
+void createTextureRectangle(double x0, double y0, double x, double y, double z);
 
 void drawEllipse(int slices, double x, double y);
 void drawFillEllipse(int slices, double x, double y);
-void createTextureTriangle(int slices, double x, double y);
 
 GLuint LoadTexture(char *FileName, int n);
 
-void setColorRGB(double red, double green, double blue);
+void setColorRGBA(double red, double green, double blue, double opacity);
 
 void reshapeSize(int w, int h);
 void createWindow(int args, char **argv, int w, int h);
+
+void setBackground();
+void renderBitmapString(float x, float y, float z, void *font, const char *string,
+                        double red, double green, double blue);
 
 #endif //COURSEWORK_BASICFIGURES_H
